aa29121bc95609ef3707b9c26a33677ab161275a
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-09-06 16:13:05 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a style={{color: "#ffffff"}} href="https://github.com/gabreuvcr" target="_blank" rel="noreferrer">Gabriel Carvalho</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola <a style={{color: "#ffffff"}} href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">DevSuperior</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
