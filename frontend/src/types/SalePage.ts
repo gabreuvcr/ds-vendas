9fd0df0646e37a8920040332ffbf558e980c5155
Gabriel Carvalho <gabreuvcr@gmail.com>
2021-09-11 18:10:07 -0300

    DataTable integration

---
+++ frontend/src/types/SalePage.ts
@@ -0,0 +1,13 @@
+import { Sale } from "./Sale";
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+};
